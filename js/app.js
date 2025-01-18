d77e470504da5a69cc413acdace04ccafe828997
Hakim <hakimalhaddad2@gmail.com>
2025-01-18 13:04:01 +0300

    Tamagotchi Homework

---
+++ js/app.js
@@ -0,0 +1,118 @@
+/*-------------------------------- Constants --------------------------------*/
+const allButtons = ['play','feed','sleep']
+const gameMessage = document.querySelector('#message')
+const resetButton = document.querySelector('#restart')
+console.log(resetButton)
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+// let number = []
+const state = ['boredom', 'hunger', 'sleepiness']
+const timer = 0;
+const gameOver = false
+/*------------------------ Cached Element References ------------------------*/
+const playBtn = document.querySelector('#play')
+const feedBtn = document.querySelector('#feed')
+const sleepBtn = document.querySelector('#sleep')
+const stat1 = document.querySelector('#boredom-stat')
+const stat2 = document.querySelector('#hunger-stat')
+const stat3 = document.querySelector('#sleepiness-stat')
+// const numbers = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10};
+
+// console.log(gameMessage)
+/*-------------------------------- Functions --------------------------------*/
+function init(){
+  timer.setInterval(runGame(), 2000);
+};
+function runGame(){}
+// console.log('the game is running!', runGame)
+
+function render(){
+  stat1.textContent = state.boredom
+  stat2.textContent = state.hunger
+  stat3.textContent = state.sleepiness
+  if 
+}
+
+function updateStates(){
+  Math.random(0, 3)
+}
+function checkGameOver(){
+  if (state === 10 > 10)
+  return gameOver === true
+}
+/*----------------------------- Event Listeners -----------------------------*/
+// playerChoice.addEventListener = ('click' , interactiveChoice)
+
+
+
+
+
+
+
+
+
+/*----------------------------- Experimented Codes -----------------------------*/
+
+// const interactiveChoice = (event) => {
+// const buttonChoice = event.target.id = 
+// }
+
+// stat1.textContent = 'test'
+// stat2.textContent = 'test2'
+// stat3.textContent = 'test3'
+
+// stat1.textContent = `${numbers}`
+// numbers.forEach((numbers,number) => {
+
+// return stat1.textContent = `${number}` });
+// for (let numbers = 0; numbers <= 10; numbers++) ;{
+//     stat1.textContent =  `${numbers}`
+// }
+// for (let i = 0; i < 9; i++) ;{
+//     stat2.textContent =  `${numbers}`
+// }
+// for (let i = 0; i < 9; i++) ;{
+//     stat3.textContent =  `${numbers}`
+// }
+
+
+// do {
+//     number = number + 1;
+//     number = number + i;
+// } while (i<5); 
+// return  stat1.textContent =  `${number}`
+
+// for (const property in numbers) 
+//     stat1.textContent = `${numbers[property]}`
+// for (let number = 0; number < 9; number++) {
+//     stat1.textContent = number
+//     console.log(number);
+//   }
+// let numbers = 1
+// let numbers2 = 2
+// let numbers3 = 3
+// let numbers4 = 4
+// let numbers5 = 5
+// let numbers6 = 6
+// let numbers7 = 7
+// let numbers8 = 8
+// let numbers9 =9
+// let numbers10 = 10
+// function init(init){
+//   timer.setInterval((runGame, 2000));
+  
+//   } ;
+//   function runGame(){
+//     console.log('the game is running!')}
+    
+//   function render() {}
+  
+  // gameOver = false
+  
+  // const runGame = ('the game is running!')
+  // console.log(runGame)
+    // for (let number = 0; number < 11; number++) {
+    //   stat1.textContent = number
+    //   console.log(number)
+    // }
